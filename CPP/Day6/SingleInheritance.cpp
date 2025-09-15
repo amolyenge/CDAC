3db98e7163001913bb03e25f9d2c49b4efb896c6
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-09-15 03:49:59 -0700

    Add files via upload

---
+++ CPP/Day6/SingleInheritance.cpp
@@ -0,0 +1,27 @@
+#include <iostream>  
+using namespace std;  
+
+ class Account {  
+   public:
+   float bonus = 2000;
+   int getBonus() 
+   {
+   		return bonus;
+   }
+ };  
+
+   class Programmer: public Account {  
+   public:  
+   float monthly_salary = 5000;  
+   void CalculateSalary()
+   {
+       cout<<"Total Salary: "<<monthly_salary+getBonus();  
+   }
+   };
+
+int main() {  
+     Programmer p1;  
+     //p1.CalculateSalary();
+        cout<<"Total Salary: "<<p1.monthly_salary;  
+    return 0;  
+}  
