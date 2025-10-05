35385a5bcb098b60828cafda4e1ad903a4d2280a
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-05 21:47:09 +0530

    amol

--- WPT/Practice/HTTP_server.js
+++ WPT/Practice/HTTP_server.js
@@ -8,3 +8,7 @@ const server = http.createServer((req , res) =>{
 server.listen(3000,() =>{
     console.log("server started on http://localhost:3000");
 });
+
+// create empty folder then npm init this will create package.json 
+// create index.js
+// to run node index.js
