dbe4f032604bce8aa682675923da4f0ab913fa1e
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-05 21:50:31 +0530

    amol

--- WPT/Practice/HTTP_server_Routes.js
+++ WPT/Practice/HTTP_server_Routes.js
@@ -18,3 +18,31 @@ const server = http.createServer((req , res) =>{
 server.listen(3000,() =>{
     console.log("server started on http://localhost:3000");
 })
+
+//-----------------------------------------------------------------------------------------------------
+
+// object showing
+
+const http = require('http');
+
+const server = http.createServer((req,res) =>{
+    res.setHeader('Content-Type','application/json');
+
+    if(req.url === '/api'){
+        const data = {
+            message:"Hello Amol",
+            status:"success",
+            time: new Date().toLocaleDateString()
+        };
+
+        res.writeHead(200);
+        res.end(JSON.stringify(data));
+    }else{
+        res.writeHead(404);
+        res.end(JSON.stringify({error: 'Page not Found'}));
+    }
+});
+
+server.listen(3000,()=>{
+    console.log("Server started");
+});
