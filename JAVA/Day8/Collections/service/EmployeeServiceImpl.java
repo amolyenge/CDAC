6db8385ecce9c3831df3c7b8ea456dd8cf6f9b0e
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-09 11:27:20 +0530

    Add files via upload

---
+++ JAVA/Day8/Collections/service/EmployeeServiceImpl.java
@@ -0,0 +1,86 @@
+package com.Collections.service;
+import com.Collections.beans.Employee;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Scanner;
+
+import com.Collections.dao.EmployeeDao;
+import com.Collections.dao.EmployeeDaoImpl;
+
+
+public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeDao edao;
+    
+    
+	public EmployeeServiceImpl() {
+		edao=new EmployeeDaoImpl();
+	}
+
+
+	@Override
+	public boolean addNewEmployee() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter id");
+		int empid=sc.nextInt();
+		System.out.println("Enter name");
+		String nm=sc.next();
+		System.out.println("enetr salary");
+		double sal=sc.nextDouble();
+		System.out.println("entr joining date (dd/mm/yyyy)");
+		String dt=sc.next();
+		LocalDate ldt=LocalDate.parse(dt,DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		Employee e=new Employee(empid,nm,sal,ldt);
+		return edao.save(e);
+	}
+
+           
+	@Override
+	public List<Employee> displayAll() {
+		return edao.findAll();
+	}
+
+
+	@Override
+	public Employee searchById(int eid) {
+		return edao.findById(eid);
+	}
+
+
+	@Override
+	public boolean deleteById(int eid) {
+		return edao.removeById(eid);
+	}
+
+
+	@Override
+	public List<Employee> findByName(String nm) {
+		return edao.findByName(nm);
+	}
+
+
+	@Override
+	public List<Employee> findBySalary(double sal) {
+		return edao.findBySal(sal);
+	}
+
+
+	@Override
+	public List<Employee> sortBySalary(double sal) {
+		return edao.sort(sal);
+	}
+
+
+	@Override
+	public boolean deleteBySalary(double sal) {
+		return edao.delBySal(sal);
+	}
+
+
+	@Override
+	public boolean updateSalary(int id, double sal) {
+		return edao.updateSal(id , sal);
+	}
+
+}
