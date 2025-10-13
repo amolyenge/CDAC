2367bc9196c2ffa5a290aacad3de90c71cf98362
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-13 11:55:21 +0530

    amol

---
+++ WPT/Day17/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2 style={{color : "red"}}>Home Page</h2>
+      <p style={{color : "red"}}>This is the main Page or Home page</p>
+    </div>
+  );
+}
+
+export default Home;
