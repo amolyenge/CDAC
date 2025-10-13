6581b7de26a42dd623bbb05b6a9b1a395809fdbb
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-13 11:36:56 +0530

    amol

---
+++ WPT/Day14/App.js
@@ -0,0 +1,17 @@
+import Lifecycle from './lifecycle';
+import Counter from './Counter';
+import CounterTable from './CounterTable';
+import TextInputL from './TextInputL';
+
+function App() {
+  return (
+    <div className="App">
+      {/* {<Lifecycle/>} */}
+      {/* {<Counter />} */}
+      {/* {<CounterTable />} */}
+      {/* {<TextInputL/>} */}
+    </div>
+  );
+}
+
+export default App;
