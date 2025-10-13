6e39d32ec1bffe1866e97099efc61284ee728adc
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-13 11:56:18 +0530

    amol

---
+++ WPT/Day17/AboutUS.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div>
+      <h2 style={{color : "green"}}>About Us</h2>
+      <p style={{color : "green"}}>This is from about US Page</p>
+    </div>
+  );
+}
+
+export default AboutUs;
