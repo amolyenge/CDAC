2b4b7abecfc0bec619a85b6558f68bd3a43a8074
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-13 11:40:53 +0530

    amol

---
+++ WPT/Day15/App.js
@@ -0,0 +1,18 @@
+import ExampleuseRef from './ExuseRefHook'
+import PTable from './Axiosexample'
+import PassGenerator from './PassGenerator';
+import LoginForm from './FormsReact';
+
+function App() {
+  return (
+    <div className="App">
+      {/* {<ExampleuseRef/>} */}
+       {/* {<DataTable/>} */}
+       {<PTable/>}
+       {/* {<PassGenerator/>} */}
+       {/* {<LoginForm/>} */}
+    </div>
+  );
+}
+
+export default App;
