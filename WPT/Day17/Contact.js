b678906f50971ede0661df856a320a9a2359b9ac
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-13 11:55:52 +0530

    amol

---
+++ WPT/Day17/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h2 style={{color : "blue"}}>Contact Us</h2>
+      <p style={{color : "blue"}}>Feel free to reach out via email or phone.</p>
+    </div>
+  );
+}
+
+export default Contact;
+
+
