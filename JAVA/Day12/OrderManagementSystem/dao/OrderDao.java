040d4076a4e7a6c033d66e1faff4b53ab40c0d33
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-16 09:11:58 +0530

    amol

---
+++ JAVA/Day12/OrderManagementSystem/dao/OrderDao.java
@@ -0,0 +1,24 @@
+package OrderManagementSys.dao;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import OrderManagementSys.beans.Customer;
+import OrderManagementSys.beans.Item;
+
+public interface OrderDao {
+
+	boolean save(Customer c, List<Item> lst);
+
+	Map<Customer, List<Item>> findAll();
+
+	Entry<Customer, List<Item>> findById(int cid);
+
+	boolean removeById(int cid);
+
+	boolean addNewItem(int cid, Item item);
+
+	boolean deleteById(int cid, int iid);
+
+}
