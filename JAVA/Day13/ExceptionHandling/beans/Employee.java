f6cf91fc174fce0a70111adfd6d1a75648a12db1
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-16 10:40:01 +0530

    amol

---
+++ JAVA/Day13/ExceptionHandling/beans/Employee.java
@@ -0,0 +1,45 @@
+package ExceptionHandling.beans;
+
+import ExceptionHandling.exceptions.NegativeSalaryException;
+
+public class Employee {
+   private int eid;
+   private String ename;
+   private double sal;
+public Employee() {
+	super();
+}
+public Employee(int eid, String ename, double sal) {
+	super();
+	this.eid = eid;
+	this.ename = ename;
+	this.sal = sal;
+}
+public int getEid() {
+	return eid;
+}
+public void setEid(int eid) {
+	this.eid = eid;
+}
+public String getEname() {
+	return ename;
+}
+public void setEname(String ename) {
+	this.ename = ename;
+}
+public double getSal() {
+	return sal;
+}
+public void setSal(double sal) throws NegativeSalaryException {
+	if(sal<0)
+		throw new NegativeSalaryException("Salary cannot be -ve");
+	else if (sal <2000)
+		throw new NegativeSalaryException("salary cannot be < 2000");
+	this.sal = sal;
+}
+@Override
+public String toString() {
+	return "Employee [eid=" + eid + ", ename=" + ename + ", sal=" + sal + "]";
+}
+   
+}
