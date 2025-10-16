5cc9ed7f3350b725de12438b398ee7a4c3b9cf1d
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-16 11:33:00 +0530

    amol

---
+++ JAVA/Day14/ObjectSerialization/Service/EmployeeService.java
@@ -0,0 +1,24 @@
+package ObjectSerialization.service;
+
+import java.util.List;
+
+import ObjectSerialization.beans.Employee;
+
+
+public interface EmployeeService {
+
+	void readFile(String fname);
+
+	boolean addNewEmployee();
+
+	boolean deleteById(int empid);
+
+	boolean modifyById(int empid, double sal);
+
+	List<Employee> getAll();
+
+	Employee getById(int empid);
+
+	void writeToFile(String string);
+
+}
