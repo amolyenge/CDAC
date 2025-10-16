25dcd78dbaf0ea0035aa29b2251855af9f373a1f
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-16 09:06:29 +0530

    amol

---
+++ JAVA/Day12/Enum/Test/TestCoffee.java
@@ -0,0 +1,22 @@
+package Enum.test;
+
+import Enum.enums.Coffee;
+
+public class TestCoffee {
+
+	public static void main(String[] args) {
+		Coffee c=Coffee.small;
+		switch(c) {
+		case small->{
+			System.out.println("Small selected "+ c.getSize()+"---->"+c.getPrice());
+		}
+		case medium->{
+			System.out.println("Medium selected"+ c.getSize()+"---->"+c.getPrice());
+		}
+		case large->{
+			System.out.println("Large selected"+ c.getSize()+"---->"+c.getPrice());
+		}
+		}
+
+	}
+}
