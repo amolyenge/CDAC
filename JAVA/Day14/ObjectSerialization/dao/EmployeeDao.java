2bc00cd82b9e92460d0f8c589d4a27ddf552e762
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-16 11:31:29 +0530

    amol

---
+++ JAVA/Day14/ObjectSerialization/dao/EmployeeDao.java
@@ -0,0 +1,26 @@
+package ObjectSerialization.dao;
+
+import java.util.List;
+
+import ObjectSerialization.beans.Employee;
+
+
+
+
+public interface EmployeeDao {
+
+	void readData(String fname);
+
+	boolean save(Employee e);
+
+	boolean removeById(int empid);
+
+	boolean updateById(int empid, double sal);
+
+	List<Employee> findAll();
+
+	Employee findById(int empid);
+
+	void writeData(String fname);
+
+}
