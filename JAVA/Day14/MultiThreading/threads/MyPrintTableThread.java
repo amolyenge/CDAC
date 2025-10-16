7290036c2e91bbd0a3976694129765666cce0d93
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-16 11:25:35 +0530

    amol

---
+++ JAVA/Day14/MultiThreading/threads/MyPrintTableThread.java
@@ -0,0 +1,20 @@
+package Multithreading.threads;
+
+import Multithreading.beans.MyClass;
+
+public class MyPrintTableThread extends Thread{
+   private int n;
+   private MyClass ob;
+   
+	
+	public MyPrintTableThread(int n, MyClass ob) {
+	super();
+	this.n = n;
+	this.ob = ob;
+}
+
+
+	public void run() {
+		ob.printTable(n);
+	}
+}
