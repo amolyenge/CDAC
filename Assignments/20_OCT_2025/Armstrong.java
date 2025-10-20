ed2a02abf990ebc4f5582046274c66fb439a3826
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-20 09:43:49 +0530

    amol

---
+++ Assignments/20_OCT_2025/Armstrong.java
@@ -0,0 +1,28 @@
+package OCT_20_2025;
+
+import java.util.Scanner;
+
+public class Armstrong {
+	public static void main(String[] args) {
+	     Scanner sc = new Scanner(System.in);
+	     System.out.println("Enter the number to check Armstrong.");
+	     int num = sc.nextInt();
+	     int original = num;
+	     int sum = 0;
+	     
+	     int digits = String.valueOf(num).length();
+	     while(num != 0) {
+	       int digit = num % 10;
+	       sum += Math.pow(digit, digits);
+	       num = num / 10;
+	     }
+	     
+	     if(sum == original) {
+	    	 System.out.println("Num is Armstrong number ..");
+	     }else {
+	    	 System.out.println("Number is Not Armstrong number..");
+	     }
+
+		}
+
+}
