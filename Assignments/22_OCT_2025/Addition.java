014ffc46aca0cc4190f64cf12eabfee3f1546719
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-22 08:35:58 +0530

    amol

---
+++ Assignments/22_OCT_2025/Addition.java
@@ -0,0 +1,19 @@
+package OCT_22_2025;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter first Number : ");
+      int num1 = sc.nextInt();
+      System.out.println("Enter second Number : ");
+      int num2 = sc.nextInt();
+      
+      int sum = num1 + num2;
+      System.out.println("Sum is : " + sum);
+
+	}
+
+}
