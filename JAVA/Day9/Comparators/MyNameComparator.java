f7decb52d2f9f918f690ac317d2b3a1b4a99223e
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-22 16:59:41 +0530

    amol

--- JAVA/Day9/Comparators/MyNameComparator.java
+++ JAVA/Day9/Comparators/MyNameComparator.java
@@ -9,8 +9,6 @@ public class MyNameComparator implements Comparator<Employee> {
 
 	@Override
 	public int compare(Employee o1, Employee o2) {
-		System.out.println("in nameComparator compare method");
-		System.out.println("Names: "+o1.getEname()+"-----"+o2.getEname());
 		return o1.getEname().compareTo(o2.getEname());
 	}
 
