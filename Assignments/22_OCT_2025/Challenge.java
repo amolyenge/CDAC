fa68f392572b142308eae9408943c71797c1f6b9
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-26 17:55:16 +0530

    amol

--- Assignments/22_OCT_2025/Challenge.java
+++ Assignments/22_OCT_2025/Challenge.java
@@ -1,37 +1,24 @@
-package OCT_22_2025;
-
-import java.util.Scanner;
 import java.util.Arrays;
 
 public class Challenge {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter size of array: ");
-        int n = sc.nextInt();
+	public static void main(String[] args) {
+      int[] nums = {0,1,3,5,6,7,8,4};
+     // Arrays.sort(nums);
+      int ans = findMissing(nums);
+      System.out.println(ans);
+	}
+	
+	public static int findMissing(int[] nums) {
+		int n = nums.length;
+		
+		int totalSum = n * (n + 1) / 2;
+		int arraySum = 0;
+		
+		for(int num : nums) {
+			arraySum += num;
+		}
+		return totalSum - arraySum;
+	}
 
-        int[] arr = new int[n];
-        System.out.println("Enter " + n + " numbers (distinct, one missing in sequence):");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        Arrays.sort(arr);
-
-        int missing = -1;
-
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i + 1] - arr[i] != 1) {
-                missing = arr[i] + 1;
-                break;
-            }
-        }
-
-        if (missing == -1) {
-            missing = arr[0] != 0 ? arr[0] - 1 : arr[n - 1] + 1;
-        }
-
-        System.out.println("\nMissing Number: " + missing);
-        sc.close();
-    }
 }
