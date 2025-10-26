c66193ac6034979293bc9860f2e1d92c119f8a43
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-26 17:31:10 +0530

    amol

--- Assignments/23_OCT_2025/challenge.java
+++ Assignments/23_OCT_2025/challenge.java
@@ -1,24 +1,37 @@
-import java.util.Arrays;
-
 public class RotateArray {
     public static void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k % n;
-        int[] temp = new int[n];
+        k = k % n; // If k > n
 
-        for (int i = 0; i < n; i++) {
-            temp[(i + k) % n] = nums[i];
-        }
-        for (int i = 0; i < n; i++) {
-            nums[i] = temp[i];
+        // Step 1: Reverse whole array
+        reverse(nums, 0, n - 1);
+
+        // Step 2: Reverse first k elements
+        reverse(nums, 0, k - 1);
+
+        // Step 3: Reverse remaining part
+        reverse(nums, k, n - 1);
+    }
+
+    // Helper function to reverse
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,6,7};
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
         int k = 3;
-
         rotate(nums, k);
-        System.out.println(Arrays.toString(nums));
+
+        System.out.print("Rotated Array: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
     }
 }
