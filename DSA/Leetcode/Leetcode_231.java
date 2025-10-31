26c097228aa032ead2a06c5e7c78c9d0c800be82
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-10-31 15:51:08 +0530

    Power of Two

---
+++ DSA/Leetcode/Leetcode_231.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (true) {
+            if (n == 1) {
+                return true;
+            }
+            if (n % 2 == 1) {
+                return false;
+            }
+            n /= 2;
+        }
+    }
+}
