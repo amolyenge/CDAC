c7ed5be6202a30b82acef710416d1428657cacb1
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-12-29 14:35:49 +0530

    Implement user login and management features

---
+++ MS.NET/Solution1/DEMO_LoginAssignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using _42Demo_ConnectedAdoNet.BLL_Business_Logic_Layer_;
+
+namespace _42Demo_ConnectedAdoNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentService service = new StudentService();
+            int choice = 0;
+           do
+            {
+                Console.WriteLine("----------Welcome to Login Page----------");
+                Console.WriteLine("1. Login User " + "2. Create User" + "3. Update Password "+ "0. Exit");
+                Console.WriteLine("ENTER your Choice...");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice) 
+                {
+                    case 1: Console.WriteLine("Enter your Username ->");
+                            string? name = Console.ReadLine().ToString();
+                            Console.WriteLine("Enter Password ->");
+                            string? password = Console.ReadLine().ToString();
+                        service.VerifyUser(name, password);
+                        break;
+                    case 2:
+                        Console.Write("Enter New Username: ");
+                        string newname = Console.ReadLine();
+                        Console.Write("Enter New Password: ");
+                        string newpassword = Console.ReadLine();
+                        service.CreateUser(newname, newpassword);
+                        break;
+
+                    case 3:
+                        Console.Write("Username: ");
+                        string user = Console.ReadLine();
+                        Console.Write("New Password: ");
+                        string newPwd = Console.ReadLine();
+                        service.UpdatePassword(user, newPwd);
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Exiting...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+
+            }while (choice != 0);
+        }
+    }
+}
